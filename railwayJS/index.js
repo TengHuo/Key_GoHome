1f5db1818dba95ce09bfd52352d8aa9865551ee0
Teng <843018739@qq.com>
2016-01-24 18:36:15 +0800

    modify train number spider to nodeJS

---
+++ railwayJS/index.js
@@ -0,0 +1,8 @@
+//向12306发送https请求
+
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // 打印google首页
+  }
+})
